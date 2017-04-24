0b67c732d6fd4f4a5812b10997751f14af23fbba
fergie1990 <ben.ferguson90@gmail.com>
2017-04-25 00:02:55 +1000

    Added comments and comleted readme

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Assignment2
 
-When there is no syncronization between the threads, the child thread does not wait for the parent thread fill the buffer before trying to print the buffer. This issue can be resolved using mutexs.
+When there is no syncronization between the threads, the child thread does not wait for the parent thread to fill the buffer before trying to print the buffer. This is because they are running concurrently on separate processors. This issue can be resolved using mutexes.
+
+Using a single mutex is not possible because the child needs to complete printing the buffer before it allows the parent to continue. The olny way to do this is to lock the parent from running that code until after the child has printed the buffer using a second mutex.
+The two mutex implementation seams to be a reliable solution.
+
+The third mutex is neccessary for the final implementation. I believe that a foruth mutex is needed to allow for full syncronization between the two threads and the order of tasks they need to complete.
--- sync.swift
+++ sync.swift
@@ -34,52 +34,63 @@ func readStdin() -> String
 //this function is used to print the buffer with the child thread
 func print(input: Arg) -> UnsafeMutableRawPointer?
 {
+	//waits for parent to read stdin
 	pthread_mutex_lock(&i.readlock)
-	//check if the structure contains something before unwrapping
+	//check the structure contains something before unwrapping
 	if input != nil 
 	{
 		let x = input!.load(as: info.self)
 		print(x.input)
 	}
-	
 	pthread_mutex_unlock(&i.readlock)
+	
+	//allow the parent continue
 	pthread_mutex_unlock(&i.enterlock)
 	
+	//waiting for notification that the user has pressed enter
 	pthread_mutex_lock(&i.exitlock)
 	print("Child thread is exiting")
 	pthread_mutex_unlock(&i.exitlock)
+	
+	//allow the parent to continue
 	pthread_mutex_unlock(&i.closelock)
 	pthread_exit(nil)
 }
 
+//***Main***
 var i = info()
 
+//initialising all the mutexes needed for future use
 pthread_mutex_init(&i.readlock, nil)
 pthread_mutex_init(&i.enterlock, nil)
 pthread_mutex_init(&i.exitlock, nil)
 pthread_mutex_init(&i.closelock, nil)
 
+//locking all the mutexes
 pthread_mutex_lock(&i.readlock)
 pthread_mutex_lock(&i.enterlock)
 pthread_mutex_lock(&i.exitlock)
 pthread_mutex_lock(&i.closelock)
 
+//create the thread inside readlock and read stdin
 pthread_create(&t, nil, print, &i)
 i.input = readStdin()
+//release readlock for child to use input
 pthread_mutex_unlock(&i.readlock)
 
+//waiting for child to print buffer
 pthread_mutex_lock(&i.enterlock)
-
 print("Please press Enter")
 repeat 
 {
 	i.input = readStdin()
 } while i.input != ""
 pthread_mutex_unlock(&i.enterlock)
+//allows child to continue
 pthread_mutex_unlock(&i.exitlock)
 
+//waiting for the child to exit
 pthread_mutex_lock(&i.closelock)
 print("Child thread is gone")
 pthread_mutex_unlock(&i.closelock)
-
 sleep(1)
