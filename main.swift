58f6fb41fcaec9afaf7fcfcb9eec34b7e0b24a00
fergie1990 <ben.ferguson90@gmail.com>
2017-05-18 08:42:23 +1000

    max buffer size working, min fill not complete

--- main.swift
+++ main.swift
@@ -17,29 +17,38 @@ struct info
 //this defines the thread and the type of Arg
 //for either macOS or linux/Ubuntu
 #if os(macOS)
-	var t1: pthread_t? = nil
-	var t2: pthread_t? = nil
+	var t: pthread_t? = nil
 	typealias Arg = UnsafeMutableRawPointer
 #else
-	var t1: pthread_t = pthread_t()
-	var t2: pthread_t = pthread_t()
+	var t: pthread_t = pthread_t()
 	typealias Arg = UnsafeMutableRawPointer?
 #endif
 
-func readNum() -> UInt16
+func readNum(val: Int32) -> [UInt16]
 {
 	var randomNum: UInt16 = 0
+	var tmpBuffer = [UInt16]()
+	var count: Int32 = 0
 	let fd = open("/dev/random", O_RDONLY)
 	if fd != -1 
 	{
-		let size = read(fd, &randomNum, MemoryLayout<UInt16>.size)
-		if size != MemoryLayout<UInt16>.size
+		while count < val 
 		{
-			print("Read failed with error:", errno)
-			exit(EXIT_FAILURE)
+			let size = read(fd, &randomNum, MemoryLayout<UInt16>.size)
+			if size != MemoryLayout<UInt16>.size
+			{
+				print("Read failed with error:", errno)
+				exit(EXIT_FAILURE)
+			}
+			else
+			{
+				tmpBuffer.append(randomNum)
+			}
+			count += 1
 		}
 	}
-	return randomNum
+	//print("tmpBuffer:", tmpBuffer)
+	return tmpBuffer
 }
 
 func constructor(size: Int32, fill: Int32) -> info
@@ -48,7 +57,7 @@ func constructor(size: Int32, fill: Int32) -> info
 	i.bufferSize = size
 	i.minFillLevel = fill
 	i.s = initialise(val: 1)
-	i.n = initialise(val: 0)
+	i.n = initialise(val: fill)
 	i.e = initialise(val: size)
 	return i
 }
@@ -72,57 +81,63 @@ func get_buffer() -> UInt16
 	return val
 }
 
-//produces a random number and inserts it in the buffer
-func producer(input: Arg) -> UnsafeMutableRawPointer?
-{
-	while(true)
-	{
-		//produce
-		let rval = readNum()
-		procure(sema: &i.e)
-		//print("procure e")
-		procure(sema: &i.s)
-		print("append")
-		print(rval)
-		//append
-		put_buffer(val: rval)
-		vacate(sema: &i.s)
-		//print("vacate s")
-		vacate(sema: &i.n)
-		//print("vacate n")
-		
-	}
-}
-
 //takes an element from the buffer and prints it
 func consumer(input: Arg) -> UnsafeMutableRawPointer?
 {
 	while(true)
 	{
 		procure(sema: &i.n)
-		//print("procure n")
+		//sleep(3)
+		//print("procure n", i.n.semval)
 		procure(sema: &i.s)
-		print("take")
+		//print("procure s take")
+		//sleep(1)
 		//take
 		let bval = get_buffer()
+		print(bval)
 		vacate(sema: &i.s)
 		//print("vacate s")
 		vacate(sema: &i.e)
-		//print("vacate e")
+		//print("vacate e", i.e.semval)
 		//consume
-		print(bval)
+		
 	}
 }
 
 //***Main***
 var i = constructor(size: 5, fill: 0)
-//create the thread inside critical section and read stdin
-i.error = pthread_create(&t1, nil, producer, &i)
-errorHandler(error: i.error)
-i.error = pthread_create(&t2, nil, consumer, &i)
-errorHandler(error: i.error)
-i.error = pthread_join(t1, nil)
-errorHandler(error: i.error)
-i.error = pthread_join(t2, nil)
+var rval = [UInt16]()
+
+// print(i.s.semval)
+// print(i.n.semval)
+// print(i.e.semval)
+while(true)
+{
+	//create the thread inside critical section and read stdin
+	i.error = pthread_create(&t, nil, consumer, &i)
+	errorHandler(error: i.error)
+	//produce
+	procure(sema: &i.e)
+	//print("procure e", i.e.semval)
+	procure(sema: &i.s)
+	//print("procure s append")
+	//append
+	//sleep(1)
+	//print(i.buffer.count)
+	if(Int32(i.buffer.count) <= i.minFillLevel)
+	{
+		rval += readNum(val: i.bufferSize - i.buffer.count)
+		//print("rval", rval)
+	}
+	put_buffer(val: rval.remove(at:0))
+	print("buffer", i.buffer)
+	vacate(sema: &i.s)
+	//print("vacate s")
+	vacate(sema: &i.n)
+	//print("vacate n", i.n.semval)
+}
+
+
+i.error = pthread_join(t, nil)
 errorHandler(error: i.error)
 clean()
